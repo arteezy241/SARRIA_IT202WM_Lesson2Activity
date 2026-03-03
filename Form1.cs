04ea5eacde253e68856dd52d41e554c5d3dff338
arteezy241 <arteezy241@gmail.com>
2026-03-03 20:58:45 +0800

    Add project files.

---
+++ Form1.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SARRIA_IT202WM_Lesson2Activity
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            dataGridView1.Rows.Add(
+                textBox8.Text,
+                textBox9.Text,
+                textBox10.Text,
+                textBox11.Text,
+                textBox12.Text,
+                textBox13.Text,
+                textBox14.Text,
+                comboBox1.SelectedItem?.ToString(),
+                pictureBox1.Image);
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog open = new OpenFileDialog();
+            open.Filter = "Image Files(*.jpg; *.jpeg; *.png)|*.jpg; *.jpeg; *.png";
+            if (open.ShowDialog() == DialogResult.OK)
+            {
+                pictureBox1.Image = new Bitmap(open.FileName);
+            
+        }
+}
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            comboBox1.Items.Add("BSCE");
+            comboBox1.Items.Add("BSME");
+            comboBox1.Items.Add("BSEE");
+            comboBox1.Items.Add("BCPE");
+            comboBox1.Items.Add("BSIE");
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            foreach (Control ctrl in this.Controls)
+            {
+                if (ctrl is TextBox)
+                {
+                    ((TextBox)ctrl).Clear();
+                }
+
+                if (ctrl is ComboBox)
+                    NewMethod(ctrl);
+            }
+
+            pictureBox1.Image = null;
+        }
+
+        private static void NewMethod(Control ctrl)
+        {
+            ((ComboBox)ctrl).SelectedIndex = -1;
+        }
+
+        private void label9_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox5_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label10_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label12_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox17_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label25_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
